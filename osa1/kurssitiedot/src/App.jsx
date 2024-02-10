4131ff46ddc9f3506c6b24f90c226bbe1ae0f5ff
mkn000 <mikkonurmr@gmail.com>
2024-02-10 19:54:25 +0200

    1.3

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -10,7 +10,7 @@ const Part = (props) => {
 	return (
 		<div>
 			<p>
-				{props.data.part} {props.data.exercises}
+				{props.data.name} {props.data.exercises}
 			</p>
 		</div>
 	)
@@ -36,18 +36,15 @@ const Total = (props) => {
 
 const App = () => {
 	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
+	const part1 = { name: 'Fundamentals of React', exercises: 10 }
+	const part2 = { name: 'Using props to pass data', exercises: 7 }
+	const part3 = { name: 'State of a component', exercises: 14 }
 
 	return (
 		<div>
 			<Header coursename={course} />
-			<Content data={[{ part: part1, exercises: exercises1 }, { part: part2, exercises: exercises2 }, { part: part3, exercises: exercises3 }]} />
-			<Total nExercises={exercises1 + exercises2 + exercises3} />
+			<Content data={[part1, part2, part3]} />
+			<Total nExercises={part1.exercises + part2.exercises + part3.exercises} />
 		</div>
 	)
 }
