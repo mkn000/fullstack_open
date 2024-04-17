6a65a7695f2863465e2c7b815ec9bd0bff7eaa42
mkn000 <mikkonurmr@gmail.com>
2024-04-17 22:19:13 +0300

    2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const FilterForm = ({ filterBy, handleFilter }) => {
   return (
@@ -46,15 +47,17 @@ const Numbers = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterBy, setFiltering] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((resp) => {
+      setPersons(resp.data);
+    });
+  }, []);
 
   const addName = (event) => {
     event.preventDefault();
