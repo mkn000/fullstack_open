27a4c12db689105d60294a0f068aac024f726cc7
mkn000 <mikkonurmr@gmail.com>
2024-05-10 23:13:52 +0300

    3.7

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 const PORT = 3001;
@@ -26,6 +28,7 @@ let persons = [
   },
 ];
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.get("/api/persons", (req, resp) => {
