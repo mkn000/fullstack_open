aa4413de9b2f29a13ac6d48018d83f735f61cdee
mkn000 <mikkonurmr@gmail.com>
2024-05-10 23:32:01 +0300

    3.8

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -28,8 +28,14 @@ let persons = [
   },
 ];
 
-app.use(morgan("tiny"));
+morgan.token("body", function (req, res) {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
+
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 app.get("/api/persons", (req, resp) => {
   resp.json(persons);
