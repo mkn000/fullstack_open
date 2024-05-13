cd90b06bac619196f2bb67a627a24ff653bf0eca
mkn000 <mikkonurmr@gmail.com>
2024-05-13 19:15:40 +0300

    3.9

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 let persons = [
   {
@@ -32,6 +33,7 @@ morgan.token("body", function (req, res) {
   if (req.method === "POST") return JSON.stringify(req.body);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
