c80bf08a3b2085bad12a171f2d9d82b319042c78
mkn000 <mikkonurmr@gmail.com>
2024-05-18 23:44:01 +0300

    3.17-3.18

--- osa3/puhelinluettelo/vite.config.js
+++ osa3/puhelinluettelo/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": { target: "http://localhost:3001", changeOrigin: true },
+      "/info": { target: "http://localhost:3001", changeOrigin: true },
     },
   },
 });
