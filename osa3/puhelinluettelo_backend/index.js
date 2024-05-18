62b665ffd7b8e75f5f926af33876b7f05dbb217f
mkn000 <mikkonurmr@gmail.com>
2024-05-18 23:08:05 +0300

    3.16

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -18,27 +18,33 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-app.get("/api/persons", (req, resp) => {
-  Contact.find({}).then((persons) => {
-    resp.json(persons);
-  });
+app.get("/api/persons", (req, resp, next) => {
+  Contact.find({})
+    .then((persons) => {
+      resp.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, resp) => {
-  Contact.findById(req.params.id).then((person) => {
-    resp.json(person);
-  });
+app.get("/api/persons/:id", (req, resp, next) => {
+  Contact.findById(req.params.id)
+    .then((person) => {
+      resp.json(person);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (req, resp) => {
-  Contact.find({}).then((persons) => {
-    resp.send(
-      `<p>Phonebook has info for ${persons.length} people</p><br/>${Date()}`,
-    );
-  });
+app.get("/info", (req, resp, next) => {
+  Contact.find({})
+    .then((persons) => {
+      resp.send(
+        `<p>Phonebook has info for ${persons.length} people</p><br/>${Date()}`,
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, resp) => {
+app.post("/api/persons", (req, resp, next) => {
   let person = req.body;
 
   if (!person.name) return resp.status(400).json({ error: "Name missing" });
@@ -47,20 +53,42 @@ app.post("/api/persons", (req, resp) => {
 
   const contact = new Contact(person);
 
-  contact.save().then((res) => {
-    console.log(`added ${res.name} number ${res.number} to phonebook`);
-    resp.json(res);
-  });
+  contact
+    .save()
+    .then((res) => {
+      console.log(`added ${res.name} number ${res.number} to phonebook`);
+      resp.json(res);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, resp) => {
+app.delete("/api/persons/:id", (req, resp, next) => {
   const id = req.params.id;
 
-  Contact.findByIdAndDelete(id).then((_) => {
-    resp.status(204).end();
-  });
+  Contact.findByIdAndDelete(id)
+    .then((_) => {
+      resp.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Listening now on port ${PORT}`);
 });
