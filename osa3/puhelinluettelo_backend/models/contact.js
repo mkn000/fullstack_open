5de1b784744d81521f5f7d4fb30d9c028b95aa94
mkn000 <mikkonurmr@gmail.com>
2024-05-19 21:56:03 +0300

    3.20

--- osa3/puhelinluettelo_backend/models/contact.js
+++ osa3/puhelinluettelo_backend/models/contact.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
   name: { type: String, minlength: 3, required: true },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /(?<!.)((\d{2}-\d{5,})|(\d{3}-\d{4,}))(?!.)/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+  },
 });
 
 contactSchema.set("toJSON", {
