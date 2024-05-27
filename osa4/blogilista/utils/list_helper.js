d77a2bea69f3f6ec796aa18cf8e636b75068887b
mkn000 <mikkonurmr@gmail.com>
2024-05-27 20:17:45 +0300

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((previous, current) => {
+    return (previous += current.likes)
+  }, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
