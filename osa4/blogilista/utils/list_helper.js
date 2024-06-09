2f95664f9686017cd9b84645b24a8970d7895f27
mkn000 <mikkonurmr@gmail.com>
2024-06-09 19:57:31 +0300

    4.6-4.7

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -11,18 +11,73 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
   if (blogs.length < 1) return null
 
-  return blogs.reduce((previous, current) => {
-    if (current.likes >= previous.likes) {
-      return current
+  return blogs.reduce(
+    (previous, current) => {
+      if (current.likes >= previous.likes) {
+        return current
+      } else {
+        return previous
+      }
+    },
+    { likes: 0 },
+  )
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length < 1) return null
+
+  const blogsByAuthor = blogs.reduce((previous, current) => {
+    if (Object.keys(previous).includes(current.author)) {
+      previous[current.author] += 1
     } else {
-      return previous
+      previous[current.author] = 1
     }
-  },
-  { likes: 0 })
+
+    return previous
+  }, {})
+
+  const authorWithMostBlogs = Object.entries(blogsByAuthor).reduce(
+    (previous, current) => {
+      return current[1] > previous[1] ? current : previous
+    },
+    ['a', 0],
+  )
+
+  return {
+    author: authorWithMostBlogs[0],
+    blogs: authorWithMostBlogs[1],
+  }
+}
+const mostLikes = (blogs) => {
+  if (blogs.length < 1) return null
+
+  const likesPerAuthor = blogs.reduce((previous, current) => {
+    if (Object.keys(previous).includes(current.author)) {
+      previous[current.author] += current.likes
+    } else {
+      previous[current.author] = current.likes
+    }
+
+    return previous
+  }, {})
+
+  const authorWithMostLikes = Object.entries(likesPerAuthor).reduce(
+    (previous, current) => {
+      return current[1] > previous[1] ? current : previous
+    },
+    ['a', 0],
+  )
+
+  return {
+    author: authorWithMostLikes[0],
+    likes: authorWithMostLikes[1],
+  }
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
