49b80fc6c731b7bb5d269df5c264fcf565afabea
mkn000 <mikkonurmr@gmail.com>
2025-03-01 22:31:02 +0200

    5.19

--- osa5/e2e_playwright/tests/blog_app.spec.js
+++ osa5/e2e_playwright/tests/blog_app.spec.js
@@ -41,4 +41,29 @@ describe("Blog app", () => {
       await expect(page.getByText("wrong credentials")).toBeVisible();
     });
   });
+
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      await page.goto("http://localhost:5173");
+      const boxes = await page.getByRole("textbox").all();
+      await boxes[0].fill("testuser");
+      await boxes[1].fill("testpw");
+      await page.getByRole("button", { name: "login" }).click();
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      await page.getByRole("button", { name: "new blog" }).click();
+      const boxes = await page.getByRole("textbox").all();
+      await boxes[0].fill("test blogtitle");
+      await boxes[1].fill("test blogauthor");
+      await boxes[2].fill("test blogurl");
+      await page.getByRole("button", { name: "create" }).click();
+
+      await expect(
+        page.locator(
+          "div:has-text('test blogtitle test blogauthor'):below(h2:has-text('blogs'))",
+        ),
+      ).toBeVisible();
+    });
+  });
 });
